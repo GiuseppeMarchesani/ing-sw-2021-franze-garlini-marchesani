68d5be93b4e2d37a2ee782f1ec29c58b2c09560c
GiuseppeMarchesani <giuseppe.marchesani.it@gmail.com>
2021-05-06 15:16:03 +0200

    added BlackCrossTest

---
+++ src/test/java/it/polimi/ingsw/model/BlackCrossTest.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlackCrossTest {
+    private BlackCross blackCross = new BlackCross();
+
+    @Test
+    public void testIncreaseBlackCross() {
+        assertEquals(blackCross.increaseBlackCross(2),2);
+        assertEquals(blackCross.increaseBlackCross(1),3);
+        assertEquals(blackCross.increaseBlackCross(4),7);
+    }
+}
