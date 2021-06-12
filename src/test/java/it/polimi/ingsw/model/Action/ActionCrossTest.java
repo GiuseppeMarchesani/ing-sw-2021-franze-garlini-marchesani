f1fdc3cd371002ac478c6e3bbcfd8ce91484751f
GiuseppeMarchesani <giuseppe.marchesani.it@gmail.com>
2021-06-12 16:31:22 +0200

    added ActionCrossTest

---
+++ src/test/java/it/polimi/ingsw/model/Action/ActionCrossTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.Action;
+
+import it.polimi.ingsw.model.SinglePlayerGame;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionCrossTest {
+    ActionCross testActionCross;
+    SinglePlayerGame testGame;
+
+    @Before
+    public void setUp() {
+        testActionCross = new ActionCross(2);
+        testGame = new SinglePlayerGame();
+    }
+
+    @Test
+    void doOperation() {
+        setUp();
+
+        testGame.getBlackCross().increaseBlackCross(10);
+        Assert.assertEquals(10, testGame.getBlackCross().getFaithSpace());
+
+        testActionCross.doOperation(testGame);
+        Assert.assertEquals(12, testGame.getBlackCross().getFaithSpace());
+
+        testActionCross.doOperation(testGame);
+        Assert.assertEquals(14, testGame.getBlackCross().getFaithSpace());
+
+        testGame.getBlackCross().increaseBlackCross(10);
+        Assert.assertEquals(24, testGame.getBlackCross().getFaithSpace());
+
+        testActionCross.doOperation(testGame);
+        Assert.assertEquals(24, testGame.getBlackCross().getFaithSpace());
+
+    }
+}
